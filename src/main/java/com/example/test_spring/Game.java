3bf3e4f1c390931f1bde8ac1e05a9c80872cd889
evin76 <gengerw1604@gmail.com>
2022-05-23 09:29:54 +0500

    поправила код к лекции 4 ConditionalOnBean в классе UserService

--- src/main/java/com/example/test_spring/Game.java
+++ src/main/java/com/example/test_spring/Game.java
@@ -2,10 +2,8 @@ package com.example.test_spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Profile;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 //field injection
